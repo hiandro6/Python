699e6f109917fac5c34a6f382fe25dda16dba001
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2023-04-27 17:04:35 -0300

    Create questao001.py

---
+++ POO/listas/lista4/questao001.py
@@ -0,0 +1,5 @@
+lista = []
+for i in ramge(0, 5):
+  num = int(input(f"adicione um número a posição {i} da lista: "))
+for i in range(0, 5):
+  print(f"o número {lista[i]} está na posição {i} da lista")
