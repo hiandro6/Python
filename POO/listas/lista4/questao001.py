0f17ac8f8c36f40d332172a720f86d1a6b532520
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2023-11-10 08:05:56 -0300

    Delete POO/listas directory

--- POO/listas/lista4/questao001.py
+++
@@ -1,5 +0,0 @@
-lista = []
-for i in ramge(0, 5):
-  num = int(input(f"adicione um número a posição {i} da lista: "))
-for i in range(0, 5):
-  print(f"o número {lista[i]} está na posição {i} da lista")
